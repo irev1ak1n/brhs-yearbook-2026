f19b49dfa13627181ed7898979cba0b394f35e6b
Illia Reviakin <illiareviakin2008@gmail.com>
2026-04-19 22:50:14 -0400

    contact us page finished for now, still have to work on it later

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,9 +1,8 @@
 document.addEventListener("DOMContentLoaded", async () => {
     const placeholder = document.getElementById("navbar-placeholder");
-
     if (!placeholder) return;
 
-    const res = await fetch("components/navbar.html");
+    const res = await fetch("/components/navbar.html");
     const html = await res.text();
     placeholder.innerHTML = html;
 
