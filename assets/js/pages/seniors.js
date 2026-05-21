8ac3942ffd2991c03e660940715f4853560efdb4
Illia Reviakin <illiareviakin2008@gmail.com>
2026-05-20 23:52:30 -0400

    added sorting for seniors

---
+++ assets/js/pages/seniors.js
@@ -0,0 +1,49 @@
+const seniorSort = document.getElementById("seniorSort");
+const seniorsList = document.querySelector(".yb-seniors-list");
+
+const originalCards = [...seniorsList.children];
+
+seniorSort.addEventListener("change", () => {
+    const cards = [...seniorsList.children];
+
+    let sortedCards = cards;
+
+    if (seniorSort.value === "default") {
+        sortedCards = originalCards;
+    }
+
+    if (seniorSort.value === "name-az") {
+        sortedCards = cards.sort((a, b) =>
+            getName(a).localeCompare(getName(b))
+        );
+    }
+
+    if (seniorSort.value === "name-za") {
+        sortedCards = cards.sort((a, b) =>
+            getName(b).localeCompare(getName(a))
+        );
+    }
+
+    if (seniorSort.value === "college-az") {
+        sortedCards = cards.sort((a, b) =>
+            getCollege(a).localeCompare(getCollege(b))
+        );
+    }
+
+    if (seniorSort.value === "college-za") {
+        sortedCards = cards.sort((a, b) =>
+            getCollege(b).localeCompare(getCollege(a))
+        );
+    }
+
+    seniorsList.innerHTML = "";
+    sortedCards.forEach(card => seniorsList.appendChild(card));
+});
+
+function getName(card) {
+    return card.querySelector("h2").textContent.trim();
+}
+
+function getCollege(card) {
+    return card.querySelector(".yb-senior-details").textContent.split("•")[0].trim();
+}
